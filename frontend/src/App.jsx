a34852a8f38976b9d3a4129629e31df01da6d3b3
will <willsandersnpt@gmail.com>
2026-02-10 21:06:04 -0500

    Basic navbar with limited styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,35 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  // const [count, setCount] = useState(0)
 
   return (
-    <div>
-      Hello World
-    </div>
+    <html>
+      <header class="main-header">
+        <div class="logo">PathExt 2.0</div>
+        <nav class="navbar">
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Run</a>
+            </li>
+            <li>
+              <a href="#">How It Works</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      <div class="description">
+        <p class="sentence">The smartest way to manage your data</p>
+      </div>
+
+    </html>
+    
   )
 }
 
