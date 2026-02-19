2023c8043988274580502c3602cd05e7df4cc275
will <willsandersnpt@gmail.com>
2026-02-18 22:11:32 -0500

    Unstyled run page form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,13 +16,13 @@ function App() {
               <a href="#">HOME</a>
             </li>
             <li>
-              <a href="#">ABOUT</a>
+              <a href="#about">ABOUT</a>
             </li>
             <li>
-              <a href="#">RUN</a>
+              <a href="#run">RUN</a>
             </li>
             <li>
-              <a href="#">HOW IT WORKS</a>
+              <a href="#hiw">HOW IT WORKS</a>
             </li>
           </ul>
         </nav>
@@ -34,6 +34,36 @@ function App() {
 
       <div class="btn-container">
         <button class="hiw-btn">HOW IT WORKS</button>
+      </div>
+
+      <div class="about-tab" id='about'>
+        About
+      </div>
+
+      <div class="run-tab" id='run'>
+        {/* <p>Run</p> */}
+
+        <form>
+          <label htmlFor="name">Name:</label>
+          <input type="text" id='name' name='name'/>
+          <label htmlFor="email">Email:</label>
+          <input type="email" name='email' id='email' required/><br/>
+          <label htmlFor="pertubation">Name of Pertubation:</label>
+          <input type="text" name='pertubation' id='pertubation'/>
+          <label htmlFor="threshold">Percentile Threshold</label>
+          <input type="text" inputMode='numeric' name='threshold' id='threshold'/><br/>
+          <label htmlFor="control">Name of Control Sample:</label>
+          <input type="text" name='control' id='control'/>
+          <label htmlFor="q-score">Q-Score Cutoff</label>
+          <input type="text" inputMode='numeric' name='q-score' id='q-score'/><br/>
+          <label htmlFor="path-length">Path Length Threshold</label>
+          <input type="text" inputMode='numeric' name='path-length' id='path-length'/><br/>
+          <input type="submit" value="Submit"/>
+        </form>
+      </div>
+
+      <div class="hiw-tab" id='hiw'>
+        How it Works
       </div>
 
     </html>
