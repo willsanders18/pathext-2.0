fe157b146f2110c9209a777fa4930654ee4dfe11
will <willsandersnpt@gmail.com>
2026-02-18 23:09:50 -0500

    About page first draft

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -37,7 +37,28 @@ function App() {
       </div>
 
       <div class="about-tab" id='about'>
-        About
+        <p class="sentence">PathExt's Story</p>
+        <p class="about-description">
+          PathExt, first coded in 2020 by [INSERT PROPER AUTHORS/CREDITS], is a
+specialized programming tool that sifts through user-uploaded genome sequencing data
+to find the most significant genes and their relations. Through datamining, discovering
+patterns/trends in large form datasets computationally, the program deconstructs the
+uploaded files and highlights which genes or ‘nodes’ are most active under the user’s
+given circumstances. Once found, the connections or ‘pathways’ between each node
+are evaluated to find the shortest distance between those of highest importance,
+mapping those ‘routes’ in turn.
+        </p>
+        <p class="sentence">Why PathExt.com</p>
+        <p class="about-description">
+          Traditional gene evaluation techniques require lengthy completion time, are arduous in
+procedure, and monetarily debilitating for the average researcher for whom they are
+designed. Our team’s solution aims to decrease the time commitment through parallel
+processing, provide a straightforward interface to operate, and remove the burden of
+cost from the user entirely. We want to make this technology more widely available for
+the researchers who want to use it, without further hindering their progress with
+complicated. Therefore, PathExt is now available in webpage format, as opposed to its
+original command-line functionality, to accomplish that very thing.
+        </p>
       </div>
 
       <div class="run-tab" id='run'>
